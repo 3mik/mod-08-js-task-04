b7da42159da2e8fe68b7de8f31af1a575c2dc110
Krzysztof Milek <krzysztof.milek73@gmail.com>
2017-12-03 17:13:34 +0100

    Modify js file - calculate triangle field

---
+++ js/script.js
@@ -0,0 +1,16 @@
+//variable declaration
+
+var a = 0,
+    h = 0,
+    triangleArea = 0;
+
+a = prompt('Enter triangle base');
+h = prompt('Enter triangle height');
+
+triangleArea = a*h/2;
+
+console.log('Triangle field base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
+
+
+
